773d056462f193c980e2239345c5641141fc847b
Kazuki Hashimoto <abkeybg111177@gmail.com>
2021-10-08 15:18:54 +0000

    [finish]いいね非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     @tweet = Tweet.find(params[:tweet_id])
     favorite = current_user.favorites.new(tweet_id: @tweet.id)
     favorite.save
-    redirect_to tweets_path
+    # redirect_to tweets_path
   end
 
   def destroy
     @tweet = Tweet.find(params[:tweet_id])
     favorite = current_user.favorites.find_by(tweet_id: @tweet.id)
     favorite.destroy
-    redirect_to tweets_path
+    # redirect_to tweets_path
   end
 end
