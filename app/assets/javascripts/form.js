d30100106fe09821cf646e0591544b3031fcfd89
Kazuki Hashimoto <abkeybg111177@gmail.com>
2021-10-25 11:38:04 +0000

    [add]counter

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,6 +1,6 @@
 /*global $*/
 $(function(){
- $("#input-text").on("keyup", function() {
+ $("#input-body").on("keyup", function() {
    var countNum = String($(this).val().length);
    $("#counter").text(countNum + "文字");
  });
