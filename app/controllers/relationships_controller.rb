5857ae3c1024c39852a3a20d690b8e779eed276f
Kazuki Hashimoto <abkeybg111177@gmail.com>
2021-11-19 14:39:31 +0000

    add

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,7 @@ class RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     if follow.save
-      # 通知機能
+      #通知機能
       @user = User.find(params[:user_id])
       @user.create_notification_follow!(current_user)
 
